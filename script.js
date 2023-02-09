6988c055a8c00174e236a3b3a3090c1f4823ff1e
danielthane <dthane8294@hotmail.co.uk>
2023-02-09 17:46:17 +0000

    game logic complete, work ongoing on reset

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const diceImage = document.querySelector('.dice');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
@@ -16,16 +19,55 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceImage.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+// Dice Roll Logic
 btnRoll.addEventListener('click', () => {
-  diceImage.classList.remove('hidden');
+  // Genertaes a new random roll
   const newRandomRoll = Math.trunc(Math.random() * 6) + 1;
+
+  //Shows the dice
+  diceImage.classList.remove('hidden');
   diceImage.src = `dice-${newRandomRoll}.png`;
 
+  // 1 Rolled Check
   if (newRandomRoll !== 1) {
     currentScore += newRandomRoll;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    switchPlayer();
   }
+});
+
+// hold Score Logic
+btnHold.addEventListener('click', () => {
+  // Add current score to the score of active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // Check if score is >= 100
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  }
+  // Finish
+  else {
+    switchPlayer();
+  }
+  //Switch to next player
 });
